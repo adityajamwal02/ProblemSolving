81ad93aa2d8d08c76bf47ac88c8e574e792ff68d
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-02-15 19:37:05 +0530

    Q1

---
+++ Week3/1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std; int main()
+{
+    char s2[100];
+    cout<<"\nEnter your second string\t";
+    gets(s2);
+    cout<<"\nYourstring is "<<s2;
+}
+
+// Alternative Way
+
+#include<bits/stdc++.h>
+using namespace std; int main()
+{
+  string s;
+  cout<<"Enter a String\t";
+  cin>>s;
+  cout<<"\nYourstring is "<<s;
+}
