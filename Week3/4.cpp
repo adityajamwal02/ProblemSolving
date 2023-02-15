fa6f66a8e06297238c45f98580102766964ad2cd
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-02-15 19:42:33 +0530

    Q4

---
+++ Week3/4.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
+#define UINT_MAX 4294967295
+
+int main()
+{
+  int i,j,k; unsigned int
+  u,v;
+  i = INT_MAX; // 2147483647 i++;
+  printf("i = %d \n", i);
+  j = INT_MIN; // -2147483648 j--;
+  printf("\nj = %d \n", j);
+  k = INT_MIN; // -2147483648
+  k = abs(k);
+  printf("\nk = %d \n", k);
+  u = UINT_MAX; // 4294967295 u++;
+  printf("\nu = %u \n", u);v = 0;
+  v--;
+  printf("\nv = %u\n", v);
+  }
