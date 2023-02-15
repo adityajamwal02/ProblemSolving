e5b43b224a2407a4fed801329ca65c73961771c3
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-02-15 19:41:07 +0530

    Q3

---
+++ Week3/3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+  char buff[15];
+  int pass=0;
+  printf("\n Enter the password : \n");
+  gets(buff);
+  if(strcmp(buff, "aditya"))
+  {
+  printf ("\n Wrong Password \n");
+  }
+  else
+  {
+  printf ("\n Correct Password \n");
+    pass = 1;
+  }
+  if(pass){
+    printf ("\n privileges given to the user \n");
+  }
+return 0;
+}
